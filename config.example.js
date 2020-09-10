8f6a33b9c42e2ba4f12b77c8d18ca45fbd16440d
Arya Antaputra <petewarrior@gmail.com>
2020-09-10 07:41:34 +0000

    config example

---
+++ config.example.js
@@ -0,0 +1,11 @@
+
+
+exports.config = {
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
+    // app_only_auth:        true,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.,
+    port: 11070,
+};
